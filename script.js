129dcb77f9be853f9641e14fff63f0beb9ac4ebd
Aizawa110723 <sj.116tlgb.pro418@gmail.com>
2024-07-09 13:38:28 +0900

    奇数偶数の判定

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const A = document.querySelector('#A');
 
+let count = 0;
+
 A.addEventListener('click', () => {
-    A.textContent = '〇';
+    count++;
+    // 奇数偶数の判定
+    if (count%2===1){
+        A.textContent = '〇';
+    } else {
+        A.textContent = '×';
+    }
 });
 
